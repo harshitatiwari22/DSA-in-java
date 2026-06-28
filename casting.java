109674b4e73ce502b87f2d7ad008a871fc16a154
harshitatiwari22 <harshitatiwari394@gmail.com>
2026-06-28 20:52:22 +0530

    Add files via upload

---
+++ casting.java
@@ -0,0 +1,11 @@
+public class casting {
+    public static void main(String[] args){
+
+        double p = 100.00;
+        double np = p + 18 ;
+        System.out.println(np);
+        
+    }
+
+    
+}
